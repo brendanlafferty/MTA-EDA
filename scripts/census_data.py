8726ade2427a5fe52975cb908b91353a6b973433
BrenBook <brendan.lafferty@gmail.com>
2020-07-02 15:17:28 -0700

    removing census_data.py

--- scripts/census_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-nyc_census_tracts = pd.read_csv('../data/nyc_census_tracts.csv')
-census_block_loc = pd.read_csv('../data/census_block_loc.csv')
-stations = pd.read_csv('../data/stations.csv')
-
-stations_with_census = pd.merge(stations, nyc_census_tracts[['CensusTract', 'Poverty']], left_on='Census Tract', right_on='CensusTract'])
